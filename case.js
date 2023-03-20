bc92c958b3f3a06941ec880097c799df4cb8bb72
haquejia <hemaljia0017@gmail.com>
2023-03-20 14:09:08 +0600

    edit in case.js file

--- case.js
+++ case.js
@@ -23,15 +23,7 @@ function updateCaseTotalNumber(newCaseNumber) {
     caseTotal.innerText = caseTotalPrice;
 }
 
-function calculateSubTotal() {
-    const currentPhoneTotal = getTextElementById('phone-total')
-    const currentCaseTotal = getTextElementById('case-total')
-    const currentSubTotal = currentPhoneTotal + currentCaseTotal;
 
-    const subTotalElement = document.getElementById('sub-total');
-    subTotalElement.innerText = currentSubTotal;
-
-}
 
 
 
